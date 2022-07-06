08202d693257867545466037da0a89074bc4cdc5
JonBrant <jonbrant@gmail.com>
2022-07-06 12:05:30 -0400

    Reorganized

---
+++ Runtime/GodelEncodeTesting.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using GodelEncoding.Core;
+using GodelEncoding.Utility;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace GodelEncoding.Runtime {
+    public class GodelEncodeTesting : MonoBehaviour {
+        [SerializeField] List<int> _values = new() {1, 2, 3};
+
+        [SerializeField]
+        private int _primeFactorizationTest = 2 * 3 * 5;
+
+        private void EnsureInit() {
+            if (_values == null) {
+                Debug.Log($"{nameof(GodelEncodeTesting)}.{nameof(EnsureInit)}() - Setting _values");
+                _values = new List<int> {1, 2};
+            }
+        }
+
+        private void OnValidate() {
+            EnsureInit();
+        }
+
+        [Button]
+        private void TestEncodeInts() {
+            string logString = "LogString";
+            IntGodelEncoding encoder = new IntGodelEncoding();
+            encoder.Encode(_values, s => logString += s);
+            //IntGodelEncoding.EncodeValues(_values, out string logString);
+            Debug.Log(logString);
+        }
+
+        [Button]
+        private void TestGetPrimesLessThan() {
+            var factorization = PureMethods.PrimeCountingFunctionInt(_primeFactorizationTest);
+            var logString = $"Primes less than {_primeFactorizationTest}: {factorization.ToCommaDelimitedString()}";
+            Debug.Log(logString);
+        }
+
+        [Button]
+        private void TestGetPrimeFactors() {
+            string log = "";
+            PureMethods.GetPrimeFactorsOf(_primeFactorizationTest, s => log += s);
+            Debug.Log(log);
+        }
+
+        [Button]
+        private void TestEncodeDecode() {
+            IntGodelEncoding encoder = new IntGodelEncoding();
+            // Encoding
+            string logString = $"GodelEncoding({_values.ToCommaDelimitedString().Replace(".", "")}) ";
+
+            string encodedValuesLog = "";
+            var encodedInt = encoder.Encode(_values, s => encodedValuesLog += s);
+            logString += $" = {encodedInt} => {encodedValuesLog} = {encodedInt}";
+
+            // Line Break
+            logString += "\n";
+
+            // Decoding
+            string decodedValuesLog = "";
+            logString += $"GodelDecoding({encodedInt})";
+        
+        
+            var decodedValues = encoder.Decode(encodedInt, s => decodedValuesLog += s);
+            logString += $" = ({decodedValues.ToCommaDelimitedString().Replace(".", "")}) ";
+            logString += $" => ";
+
+
+            // Operator
+            string x = "g";
+            logString += $"({PureMethods.GetPrimeFactorsOf(encodedInt, s => { }).ToCommaDelimitedString().Replace(".", "")})";
+            logString += $" {nameof(x)} ";
+            logString += $"({decodedValues.ToCommaDelimitedString().Replace(".", "")}) ";
+
+
+
+            logString += $"\n";
+            var godelOperatorOutput = GodelOperations.GodelOperatorString(decodedValues, PureMethods.GetPrimeFactorsOf(encodedInt, s => { }));
+            logString += $"GodelOperatorString: {godelOperatorOutput}";
+
+
+            logString += $"\n";
+
+            Func<string, string, string> stringExponentiation = (s1, s2) => $"{s1}^{s2}";
+            Func<string, string, string> stringMultiplication = (s1, s2) => stringExponentiation.Invoke($"( [{s1}]", $"[{s2}] ) ");
+
+            var output = GodelOperations.GodelOperator<string>(decodedValues, PureMethods.GetPrimeFactorsOf(encodedInt, s => { }), stringExponentiation,
+                stringMultiplication);
+            logString += $"GenericGodelOperator: {output}";
+
+            Debug.Log(logString);
+        }
+    }
+}
---
+++ Utility/GodelOperations.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Godel Operator
+// Dot product? Does more than a dot product. Like a dot product equipped with a Func.
+// ToDo: https://www.wikiwand.com/en/G%C3%B6del_operation - See if related
+// ToDo: GodelOperator(List<int> values, List<int> alphabet, Func<int, int, int> exponentiation, Func<int, int, int>)
+namespace GodelEncoding.Utility {
+    public static class GodelOperations {
+        public static string GodelOperator<T>(List<int> values, List<int> alphabet, Func<string, string, string> inner, Func<string, string, T> outer) {
+
+            Debug.Assert(values.Count == alphabet.Count);
+            string returnString = "";
+
+            returnString += $"({values.ToCommaDelimitedString().Replace(".", "")})";
+            returnString += $" x ";
+            returnString += $"({alphabet.ToCommaDelimitedString().Replace(".", "")})";
+            returnString += $" => ";
+
+            string outputString = "";
+            for (int i = 0; i < values.Count; i++) {
+                var s = outer.Invoke(inner.Invoke(alphabet[i].ToString(), values[i].ToString()),
+                    inner.Invoke(alphabet[i].ToString(), values[i].ToString()));
+                outputString += s;
+            }
+
+            returnString += outputString;
+
+
+            return returnString;
+        }
+
+        public static string GodelOperator(List<int> values, List<int> alphabet) {
+
+            Func<string, string, string> stringExponentiation = (s1, s2) => $"{s1}^{s2}";
+            Func<string, string, string> stringMultiplication = (s1, s2) => stringExponentiation.Invoke($"( [{s1}]", $"[{s2}] ) ");
+
+            Debug.Assert(values.Count == alphabet.Count);
+            string returnString = "";
+
+            returnString += $"({values.ToCommaDelimitedString().Replace(".", "")})";
+            returnString += $" x ";
+            returnString += $"({alphabet.ToCommaDelimitedString().Replace(".", "")})";
+
+            returnString += $" => ";
+
+            string outputString = "";
+
+            for (int i = 0; i < values.Count; i++) {
+                outputString += stringMultiplication.Invoke(alphabet[i].ToString(), values[i].ToString());
+            }
+
+            /*
+        string outputString = "";
+        for (int i = 0; i < values.Count; i++) {
+            outputString += $" ([{alphabet[i]}]^[{values[i]}]) ";
+        }
+        */
+
+            returnString += outputString;
+
+
+            return returnString;
+        }
+
+        public static string GodelOperatorString(List<int> values, List<int> alphabet) {
+            Debug.Assert(values.Count == alphabet.Count);
+            string returnString = "";
+
+            returnString += $"({values.ToCommaDelimitedString().Replace(".", "")})";
+            returnString += $" x ";
+            returnString += $"({alphabet.ToCommaDelimitedString().Replace(".", "")})";
+
+            returnString += $" => ";
+
+            string outputString = "";
+
+            for (int i = 0; i < values.Count; i++) {
+                outputString += $" ([{alphabet[i]}]^[{values[i]}]) ";
+            }
+
+            returnString += outputString;
+
+
+            return returnString;
+        }
+    }
+}
